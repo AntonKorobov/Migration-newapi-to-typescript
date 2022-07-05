5034d6bf1ff6f38726f0ef9e52d4af427889fda0
NewAnton <1anton1korobov@gmail.com>
2022-07-05 21:18:04 +0300

    fix: logo wrap when not enough spase fix: url in appLoader section

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '27bb0e3eda244d1e802bc5e979a8e1ca', // получите свой ключ https://newsapi.org/
         });
     }
